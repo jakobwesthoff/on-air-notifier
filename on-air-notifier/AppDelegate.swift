34b4b3e1060503e5154086decb5a3289db6d4251
Jakob Westhoff <jakob@westhoffswelt.de>
2019-12-04 09:47:09 +0100

    Initial Commit

---
+++ on-air-notifier/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  on-air-notifier
+//
+//  Created by Jakob Westhoff on 04.12.19.
+//  Copyright © 2019 Jakob Westhoff. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
